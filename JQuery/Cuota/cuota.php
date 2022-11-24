e2fb36a0732ba3b7211b9779099e7051bdaaf419
JaviAkaSino <javiakasino@hotmail.com>
2022-11-24 20:42:06 +0100

    c

--- JQuery/Cuota/cuota.php
+++ JQuery/Cuota/cuota.php
@@ -1,6 +1,6 @@
 <?php
 $capital = $_POST["capital"];
-$interes = $_POST["interes"]/12;
+$interes = $_POST["interes"]/12; //Divide entre 12 para ser mensual
 $plazo = $_POST["plazo"];
 
 $cuota = $capital / ((1 - pow((1 + $interes), -$plazo)) / $interes);
